57280928cba99277659c0e5de8a084125e1f5b8c
Ziad Abbas <tt933825@gmail.com>
2025-04-26 22:36:36 +0300

    Added CORS

--- server.js
+++ server.js
@@ -2,6 +2,9 @@ require("dotenv").config();
 const express = require("express");
 const mongoose = require("./db");
 const app = express();
+const cors = require('cors');
+
+app.use(cors());
 app.use(express.json());
 
 
